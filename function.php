58b40751bd0034f1b9d0a802b8f70450bf7d8de9
max <tahamohamedfr@gmail.com>
2018-06-13 11:30:27 +0200

    php

---
+++ function.php
@@ -0,0 +1,8 @@
+<?php 
+echo "\n","<<<<<<<<<<<<<<<<<<<<<php-exercises- Les fonctions>>>>>>>>>>>>>>>>>>>";
+echo "\n","//////////////////////Exercice 1 >>///////////////////","\n";
+function retourne_true()
+{
+	echo return TRUR;
+}
+?>
---
+++ variable.php
@@ -0,0 +1,67 @@
+
+ <?php 
+ $nom = " TAHA ";
+ $prenom = " Max ";
+ $age =26;
+
+ echo "<<<<<<<<<<<<<<<<<<<<<php-exercises-variable >>>>>>>>>>>>>>>>>>>","\n";
+ echo "\n","//////////////////////Exercice 1 ///////////////////","\n";
+ echo "Hello PHP!","\n";  
+
+ echo "\n","Exercice 2 ///////////////////","\n";
+ echo "\n","your name is " ,$nom,"\n","your prenom is ",$prenom,"\n","your age is ",$age ;
+
+ echo "\n","Exercice 3 ///////////////////","\n";
+ $km = 1;
+ echo "\n";
+ echo "une variable km = ",$km;
+ $km = 3;
+ echo "\n";
+ echo "une variable km = ",$km;
+
+ $km = 125;
+ echo "\n";
+ echo "une variable km = ",$km;
+
+ echo "\n","/Exercice 4 ///////////////////","\n","\n";
+
+ $string = (string)"Les variables";
+ $integer = (integer)123;
+ $float =(float)1.23;
+ $booléan = true;
+
+echo "une variable de type string = ",$string,"\n";
+echo "une variable de type integer = ",$integer,"\n";
+echo "une variable de type float = ",$float,"\n";
+echo "une variable de type booléan = ",$booléan,"\n";
+
+echo "\n","Exercice 5 ///////////////////","\n","\n";
+$ss = (integer)true;
+echo  "la variable de type int avec rien  =",$ss,"\n";
+$ss =(integer)123;
+echo  "la variable de type int avec une valeur   =",$ss,"\n";
+
+echo "\n","Exercice 6 ///////////////////","\n","\n";
+
+echo "Bonjour M." ,$nom, "  comment vas tu";
+
+echo "\n","Exercice 7 ///////////////////","\n","\n";
+
+ $nom = "TAHA";
+ $prenom = " Max "; 
+ $age = 0x1A;
+
+
+echo "Bonjour M.",$nom," ",$prenom,"tu as ",$age," ans";
+echo "\n","Exercice 8 ///////////////////","\n","\n";
+
+$x = 3+4; 
+$y = 5*20;
+$z = 45/5;
+
+echo "le résultat de l'opération 3 + 4 = ",$x,"\n";
+echo "le résultat de l'opération 5*20 = ",$y,"\n";
+echo "le résultat de l'opération 45/5 = ",$z,"\n";
+echo "don......";
+?>
+       
